26d75e73f69fd8d82f9104b0c390bd12d8827ed5
Tran Vu Binh <tranb4010@gmail.com>
2024-05-31 19:39:05 +0700

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
 import Button from '@mui/material/Button'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+
 
 function App() {
 
   return (
     <>
+      <h1>My App</h1>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <br></br>
+      <HomeIcon />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[500] }} />
     </>
   )
 }
